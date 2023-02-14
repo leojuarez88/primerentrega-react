62fb6eb36e34296366a9b30b8ac73f2463d88f7d
ljuarez <leojuarez1988@gmail.com>
2023-02-14 20:18:34 -0300

    entrega final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
 
-    NavBar
+      
+      <NavBar/>
 
-    ItemListContainer
+      <ItemListContainer/> 
+
+    
+        
       
     </div>
   );
---
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const ItemListContainer =() => {
+    return  <div className="container card">
+      <div class="card-body ">
+      <img src="./Imagen Inicio 10 Mejorada.jpg" class="card-img-top sm" alt="..."></img>  
+      <h5 class="card-title">Veladores Multicolor x 4 unidades</h5>
+      <p class="card-text">Modelo para oficinas modernas.</p>
+      <a href="#" class="btn btn-primary">Comprar</a>
+    </div>
+      
+  </div>
+};
+
+
+export default ItemListContainer;
