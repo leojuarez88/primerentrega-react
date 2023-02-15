4f2d327f317351baab25e1eddeabb361131f4a59
ljuarez <leojuarez1988@gmail.com>
2023-02-14 22:44:17 -0300

    Ultima revision

--- src/Components/ItemListContainer/ItemListContainer.js
+++ src/Components/ItemListContainer/ItemListContainer.js
@@ -5,7 +5,7 @@ const ItemListContainer =() => {
     return  <div className="container card">
       <div class="card-body ">
       <img src="./Imagen Inicio 10 Mejorada.jpg" class="card-img-top sm" alt="..."></img>  
-      <h5 class="card-title">Veladores Multicolor x 4 unidades</h5>
+      <h1 class="card-title">Veladores Multicolor x 4 unidades</h1>
       <p class="card-text">Modelo para oficinas modernas.</p>
       <a href="#" class="btn btn-primary">Comprar</a>
     </div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,40 +1,62 @@
 import React from "react";
 
-
-
-const NavBar =() => {
-    return <nav class="navbar navbar-expand-lg bg-body-tertiary p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 bs-primary-fgb">
-        <div class="container-fluid">
-      <a class="navbar-brand" href="#">Light-Vel S.A.</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">INICIO</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">PRODUCTOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">SOBRE NOSOTROS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">FORMAS DE PAGO</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">CONTACTO</a>
-          </li>
-        </ul>
-        <div class="">
-        <img src="./NavBar/Cartwidget.jpg" class="" alt="..."></img>  
-          
+const NavBar = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 bs-primary-fgb">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Light-Vel S.A.
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarText"
+          aria-controls="navbarText"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">
+                INICIO
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                PRODUCTOS
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                SOBRE NOSOTROS
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                FORMAS DE PAGO
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                CONTACTO
+              </a>
+            </li>
+          </ul>
+          <div>
+          <div>
+        
+        <img src="./Cartwidget.jpg" width="50"></img>
+            
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-
+    </nav>
+  );
 };
 
 export default NavBar;
